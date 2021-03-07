24f39b7eb59cf7ce2b8b4da66d6a3b8827eee79c
Othmane Es-safi <44867969+Othmaneessafi@users.noreply.github.com>
2021-03-07 23:23:43 +0100

    Update profile.js

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import StepContent from '@material-ui/core/StepContent';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Infos from './infos';
-import Photos from './photos';
-import Localisation from './Localisation';
+import React from "react";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Infos from "./infos";
+import Photos from "./photos";
+import Localisation from "./Localisation";
 import "./profile.css";
+import Grid from "@material-ui/core/Grid";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 function getSteps() {
-  return ['Information', 'Photos', 'Localisation'];
+  return ["Information", "Photos", "Localisation"];
 }
 
 function getStepContent(step) {
@@ -24,11 +25,11 @@ function getStepContent(step) {
     case 2:
       return <Localisation />;
     default:
-      return 'Unknown step';
+      return "Unknown step";
   }
 }
 
-export default function Profile() {
+export default function Profile(props) {
   const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
 
@@ -45,19 +46,77 @@ export default function Profile() {
   };
 
   return (
-    <div className="profileContainer">
-      <Stepper activeStep={activeStep} orientation="vertical" className="stepperContainer">
-        {steps.map((label, index) => (
-          <Step key={label}>
-            <StepLabel className="stepLabel">{label}</StepLabel>
-            <StepContent>
-              <Typography>{getStepContent(index)}</Typography>
-              <div >
-                <div>
+    <>
+      <Grid
+        container
+        className="profilContainer"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={7} container className="profileContainer">
+          <Grid
+            item
+            xs={12}
+            lg={3}
+            className="profileImageContainer"
+            container
+            justify="center"
+            alignItems="center"
+          >
+            <Stepper
+              activeStep={activeStep}
+              orientation="vertical"
+              className="stepperContainer"
+            >
+              {steps.map((label, index) => (
+                <Step key={label}>
+                  <StepLabel className="stepLabel">
+                    <h2>{label}</h2>
+                  </StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+          </Grid>
+
+          <Grid
+            item
+            container
+            xs={12}
+            lg={9}
+            className="profileInputContainer"
+            justify="center"
+            alignItems="center"
+          >
+            {activeStep === steps.length ? (
+              <div>
+                <Typography>All steps completed</Typography>
+                <Button onClick={handleReset}>Reset</Button>
+              </div>
+            ) : (
+              <Grid
+                container
+                item
+                sm={12}
+                justify="center"
+                alignItems="center"
+                spacing={4}
+              >
+                <Grid
+                  container
+                  item
+                  sm={10}
+                  justify="center"
+                  alignItems="center"
+                >
+                  {getStepContent(activeStep)}
+                </Grid>
+                <Grid container item sm={6} justify="center"
+                alignItems="center">
                   <Button
+                    color="secondary"
                     disabled={activeStep === 0}
                     onClick={handleBack}
-                    color="secondary"
+                    style={{color: "#C51162"}}
                   >
                     Back
                   </Button>
@@ -65,23 +124,28 @@ export default function Profile() {
                     variant="contained"
                     color="secondary"
                     onClick={handleNext}
+                    className="profileBtn"
                   >
-                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                    <ArrowForwardIosIcon className="forward" />
                   </Button>
-                </div>
-              </div>
-            </StepContent>
-          </Step>
-        ))}
-      </Stepper>
-      {activeStep === steps.length && (
-        <Paper square elevation={0} className="finish">
-          <Typography>Congratulations you finish</Typography>
-          <Button onClick={handleReset} color="secondary">
-            Reset
-          </Button>
-        </Paper>
-      )}
-    </div>
+                </Grid>
+              </Grid>
+            )}
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <div className="profileContainer">
+        {activeStep === steps.length && (
+          <Paper square elevation={0} className="finish">
+            <Typography>Congratulations you finish</Typography>
+            <Button onClick={handleReset} color="secondary">
+              Reset
+            </Button>
+          </Paper>
+        )}
+      </div>
+    </>
   );
 }
