42eb450e792da046c624de8f5061860666ff7386
Othmane Es-safi <44867969+Othmaneessafi@users.noreply.github.com>
2021-03-07 23:24:10 +0100

    Update photos.js

--- client/src/components/profile/photos.js
+++ client/src/components/profile/photos.js
@@ -1,9 +1,70 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import AddBtn from "./img/plus.svg";
 
 export default function photos() {
   return (
-    <div>
-      <div className="infosContainer"></div>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item container sm={4} className="PhotosContainer">
+        <Grid
+          item
+          container
+          sm={12}
+          className="userImage"
+          justify="flex-end"
+          alignItems="flex-end"
+        >
+          <img src={AddBtn} className="addBtn" alt="userImage" />
+        </Grid>
+      </Grid>
+      <Grid item container sm={4} className="PhotosContainer">
+        <Grid
+          item
+          container
+          sm={12}
+          className="userImage"
+           justify="flex-end"
+          alignItems="flex-end"
+        >
+          <img src={AddBtn} className="addBtn" alt="userImage" />
+        </Grid>
+      </Grid>
+      <Grid item container sm={4} className="PhotosContainer">
+        <Grid
+          item
+          container
+          sm={12}
+          className="userImage"
+           justify="flex-end"
+          alignItems="flex-end"
+        >
+          <img src={AddBtn} className="addBtn" alt="userImage" />
+        </Grid>
+      </Grid>
+      <Grid item container sm={4} className="PhotosContainer">
+        <Grid
+          item
+          container
+          sm={12}
+          className="userImage"
+           justify="flex-end"
+          alignItems="flex-end"
+        >
+          <img src={AddBtn} className="addBtn" alt="userImage" />
+        </Grid>
+      </Grid>
+      <Grid item container sm={4} className="PhotosContainer">
+        <Grid
+          item
+          container
+          sm={12}
+          className="userImage"
+           justify="flex-end"
+          alignItems="flex-end"
+        >
+          <img src={AddBtn} className="addBtn" alt="userImage" />
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
