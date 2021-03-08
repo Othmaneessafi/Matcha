0ecca4672964547bc0b12b38f4a7ad0bb80dc8e3
Othmane Es-safi <44867969+Othmaneessafi@users.noreply.github.com>
2021-03-08 01:14:11 +0100

    Update Localisation.js

--- client/src/components/profile/Localisation.js
+++ client/src/components/profile/Localisation.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-export default function Localisaton() {
+
+const Localisation = (props) => {
   return (
     <>
-      <div className="infosContainer"></div>
+      <Grid container spacing={2} justify="center" alignItems="center">
+        <Grid item container sm={6} className="MapContainer">
+          <div>
+        <Map
+        google={props.google}
+        zoom={14}
+        className="map"
+      /></div>
+        </Grid>
+      </Grid>
     </>
   );
 }
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCSfDV8hhu4QIgizIVhE6HP4Iu9mbRlCe8'
+})(Localisation);
