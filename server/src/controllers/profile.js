0116e83dd233903b606213f940992ee928d22824
smoudene <58786816+smoudene@users.noreply.github.com>
2021-03-10 18:27:32 +0100

    update

--- server/src/controllers/profile.js
+++ server/src/controllers/profile.js
@@ -2,43 +2,21 @@ const bcrypt = require("bcrypt");
 const crypto = require('crypto');
 const uti = require('../util/lib');
 const user = require('../models/user');
-const sendmail = require('./sendmail');
-const saltRounds = 10;
 
 profile = async (req, res) => {
-    const {id, first_name, last_name} = req.body;
-    console.log(last_name);
-    // let GetUserByUsername = await user.getUser('GetUserByUsername', username);
-    // let GetUserByEmail = await user.getUser('GetUserByEmail', email);
+    const {id, first_name, bio, last_name, birth , gender, intrest} = req.body;
     let data = {
         isValid: true,
         errUsername: null,
         errEmail: null,
     };
-   // user.updateInfos(first_name , last_name, [id])
-    user.updateUser([first_name], [last_name], [id])
+    user.updateUser([first_name], [last_name], [bio], [birth],[gender], [intrest], [id])
     .then((response) => {
+        console.log("updated")
           res.send(response);
-          console.log("wa333")
     }).catch((error) => {
           console.log(error);
       }); 
-    // if (GetUserByEmail) {
-    //     data.errEmail = 'Email already exists';
-    // }
-    // if (GetUserByUsername) {
-    //     data.errUsername = 'Username already exists';
-    // }
-    // if (!uti.isUsername(username) || !uti.isEmail(email) || GetUserByEmail || GetUserByUsername || !uti.isPassword(password, confirmPassword)) {
-    //     data.isValid = false;
-    // }
-    // else {
-    //     let hashPassword = await bcrypt.hash(password, saltRounds);
-    //     const vfToken = crypto.randomBytes(64).toString('hex');
-    //     user.Register( username, email, hashPassword);
-    //     user.UpdatvfToken(email, vfToken);
-    //     sendmail.sendEmail(email, vfToken);
-    // }
     res.send(data);
 };
 module.exports = profile;
